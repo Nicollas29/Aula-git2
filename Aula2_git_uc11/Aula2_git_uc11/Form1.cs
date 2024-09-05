680005306cc943aa9fb420b93e3a5a5acf4cbbf4
NICOLLAS LOPES MAXIMIANO <nicollas.lmaximiano@senacsp.edu.br>
2024-09-05 20:46:08 -0300

    Boa noite tropa do grau

---
+++ Aula2_git_uc11/Aula2_git_uc11/Form1.cs
@@ -0,0 +1,11 @@
+namespace Aula2_git_uc11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("Testando");
+        }
+    }
+}
